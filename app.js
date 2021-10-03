84bb4105bf988b1c9850ab0a897ed2e5e8f68d3d
mattgefen <matt.gefen@gmail.com>
2021-10-03 15:09:44 -0600

    minimum brief met, bonus to come

--- app.js
+++ app.js
@@ -9,9 +9,10 @@
 
 const game = {
   title: 'Guess the Number!',
-  biggestNum: 3,
+  biggestNum: 5,
   smallestNum: 1,
   secretNum: null,
+  playerGuess: null,
   prevGuesses: [],
   getGuess: function() {
     let guess = NaN
@@ -24,17 +25,28 @@ const game = {
     }
     return guess
   },
+  render: function() {
+    let prevGuessesString = this.prevGuesses.join(', ')
+    if (this.secretNum > this.playerGuess) {
+      alert(`Your guess is too low. Previous guesses: ${prevGuessesString}`)
+    }
+    else if (this.secretNum < this.playerGuess) {
+      alert(`Your guess is too high. Previous guesses: ${prevGuessesString}`)
+    }
+    else {
+      alert(`Congrats! You guessed the number in ${this.prevGuesses.length}`)
+    }
+  },
   play: function() {
     this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + 1
-    let playerGuess = NaN
-    while (playerGuess != this.secretNum) {
-      playerGuess = this.getGuess()
-      this.prevGuesses.push(playerGuess)
+    this.playerGuess = NaN
+    while (this.playerGuess != this.secretNum) {
+      this.playerGuess = this.getGuess()
+      this.prevGuesses.push(this.playerGuess)
+      this.render()
     }
-    return this.prevGuesses
-    
-    }
-  }
+  },
+}
 
 
-console.log(game.play())
+game.play()
