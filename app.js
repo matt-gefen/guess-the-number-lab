496dca738e4f52072c7046abbf7408ae9b84e76e
mattgefen <matt.gefen@gmail.com>
2021-10-03 15:11:34 -0600

    comment removal

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-// Features:
-// 1. Allow the player to be prompted to guess at the secret number until they guess correctly
-// 2. If the player has an incorrect guess, display an alert message that informs the player:
-  // whether the guess is too high or too low
-  // A list of all preciously guessed numbers (without array brackets)
-// If the player has guessed the secret number:
-  //  Display an alert that congratulates and informs total guesses
-  // End the gameplay
-
 const game = {
   title: 'Guess the Number!',
   biggestNum: 5,
